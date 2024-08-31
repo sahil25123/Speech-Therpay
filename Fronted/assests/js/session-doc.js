bc1cba016f2c1b3b5cb361f96f65f344cdfb159d
futuristic-coder <raman17100100@gmail.com>
2024-08-31 15:23:08 +0530

    doc

--- Fronted/assests/js/session-doc.js
+++ Fronted/assests/js/session-doc.js
@@ -7,16 +7,6 @@ document.addEventListener("DOMContentLoaded", function() {
         sidebar.classList.toggle("closed");
         content.classList.toggle("shifted");
     });
-})
-document.addEventListener("DOMContentLoaded", function() {
-    const sidebar = document.getElementById("sidebar");
-    const content = document.getElementById("content");
-    const sidebarToggle = document.getElementById("sidebarToggle");
-
-    sidebarToggle.addEventListener("click", function() {
-        sidebar.classList.toggle("closed");
-        content.classList.toggle("shifted");
-    });
 
     document.getElementById("generatePDF").addEventListener("click", function() {
         const { jsPDF } = window.jspdf;
